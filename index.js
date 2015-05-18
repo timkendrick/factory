8953b62a8f9722e45cb9898dd1d522f4fe245d40
Tim Kendrick <timkendrick@gmail.com>
2015-05-18 01:21:46 +0100

    Move helper functions into main function

--- index.js
+++ index.js
@@ -59,113 +59,113 @@ module.exports = function(options) {
 
 		return emitter;
 	};
+
+
+	function parseOptions(context, placeholders) {
+		return new Promise(function(resolve, reject) {
+			var prompts = placeholders.filter(function(option) {
+				var optionName = option.name;
+				return !context.hasOwnProperty(optionName);
+			});
+
+			if (prompts.length === 0) {
+				return resolve(context);
+			}
+
+			inquirer.prompt(prompts, function(answers) {
+				context = extend(context, answers);
+				return resolve(context);
+			});
+		});
+	}
+
+	function ensureValidSource(path) {
+		return new Promise(function(resolve, reject) {
+			fs.stat(path, function(error, stats) {
+				if (error) {
+					return reject(error);
+				}
+				if (!stats.isDirectory()) {
+					return reject(fsError('ENOTDIR', path));
+				}
+				return resolve();
+			});
+		});
+	}
+
+	function copyDirectory(source, destination, context, options, emitter) {
+		var copier = copy(source, destination, {
+			overwrite: options.overwrite,
+			dot: true,
+			rename: function(filePath) {
+				return expandPlaceholders(filePath, context);
+			},
+			transform: function(src, dest, stats) {
+				return templateStream(src, context);
+			}
+		});
+		return addEventListeners(copier, emitter);
+
+
+		function templateStream(filename, context) {
+			var stream = new Transform({ decodeStrings: false, encoding: 'utf8' });
+			stream._transform = function(chunk, encoding, done) {
+				var isBuffer = Buffer.isBuffer(chunk);
+				var isTextFile = !isBuffer || istextorbinary.isTextSync(filename, chunk);
+				if (isTextFile) {
+					var templateString = isBuffer ? chunk.toString() : chunk;
+					var containsPlaceholders = templateString.indexOf('<%') !== -1;
+					if (containsPlaceholders) {
+						try {
+							chunk = template(templateString)(context);
+						} catch (error) {
+							done(error);
+						}
+						encoding = 'utf8';
+					}
+				}
+				this.push(chunk, encoding);
+				done();
+			};
+			return stream;
+		}
+
+		function expandPlaceholders(templateString, context) {
+			var containsPlaceholders = templateString.indexOf('<%') !== -1;
+			if (!containsPlaceholders) { return templateString; }
+			var templateFunction = template(templateString);
+			return templateFunction(context);
+		}
+
+		function addEventListeners(copier, emitter) {
+			var eventNames = Object.keys(copy.events).map(function(key) {
+				return copy.events[key];
+			});
+			eventNames.forEach(function(eventName) {
+				var listener = createEventListener(eventName);
+				copier.on(eventName, listener);
+			});
+			return copier;
+
+
+			function createEventListener(eventName) {
+				return function(args) {
+					var eventArguments = Array.prototype.slice.call(arguments);
+					emitter.emit.apply(emitter, [eventName].concat(eventArguments));
+				};
+			}
+		}
+	}
+
+	function fsError(code, path) {
+		var errorType = errno.code[code];
+		var message = errorType.code + ', ' + errorType.description + ' ' + path;
+		var error = new Error(message);
+		error.errno = errorType.errno;
+		error.code = errorType.code;
+		error.path = path;
+		return error;
+	}
 };
 
 module.exports.events = copy.events;
-
-
-function parseOptions(context, placeholders) {
-	return new Promise(function(resolve, reject) {
-		var prompts = placeholders.filter(function(option) {
-			var optionName = option.name;
-			return !context.hasOwnProperty(optionName);
-		});
-
-		if (prompts.length === 0) {
-			return resolve(context);
-		}
-
-		inquirer.prompt(prompts, function(answers) {
-			context = extend(context, answers);
-			return resolve(context);
-		});
-	});
-}
-
-function ensureValidSource(path) {
-	return new Promise(function(resolve, reject) {
-		fs.stat(path, function(error, stats) {
-			if (error) {
-				return reject(error);
-			}
-			if (!stats.isDirectory()) {
-				return reject(fsError('ENOTDIR', path));
-			}
-			return resolve();
-		});
-	});
-}
-
-function copyDirectory(source, destination, context, options, emitter) {
-	var copier = copy(source, destination, {
-		overwrite: options.overwrite,
-		dot: true,
-		rename: function(filePath) {
-			return expandPlaceholders(filePath, context);
-		},
-		transform: function(src, dest, stats) {
-			return templateStream(src, context);
-		}
-	});
-	return addEventListeners(copier, emitter);
-
-
-	function templateStream(filename, context) {
-		var stream = new Transform({ decodeStrings: false, encoding: 'utf8' });
-		stream._transform = function(chunk, encoding, done) {
-			var isBuffer = Buffer.isBuffer(chunk);
-			var isTextFile = !isBuffer || istextorbinary.isTextSync(filename, chunk);
-			if (isTextFile) {
-				var templateString = isBuffer ? chunk.toString() : chunk;
-				var containsPlaceholders = templateString.indexOf('<%') !== -1;
-				if (containsPlaceholders) {
-					try {
-						chunk = template(templateString)(context);
-					} catch (error) {
-						done(error);
-					}
-					encoding = 'utf8';
-				}
-			}
-			this.push(chunk, encoding);
-			done();
-		};
-		return stream;
-	}
-
-	function expandPlaceholders(templateString, context) {
-		var containsPlaceholders = templateString.indexOf('<%') !== -1;
-		if (!containsPlaceholders) { return templateString; }
-		var templateFunction = template(templateString);
-		return templateFunction(context);
-	}
-
-	function addEventListeners(copier, emitter) {
-		var eventNames = Object.keys(copy.events).map(function(key) {
-			return copy.events[key];
-		});
-		eventNames.forEach(function(eventName) {
-			var listener = createEventListener(eventName);
-			copier.on(eventName, listener);
-		});
-		return copier;
-
-
-		function createEventListener(eventName) {
-			return function(args) {
-				var eventArguments = Array.prototype.slice.call(arguments);
-				emitter.emit.apply(emitter, [eventName].concat(eventArguments));
-			};
-		}
-	}
-}
-
-function fsError(code, path) {
-	var errorType = errno.code[code];
-	var message = errorType.code + ', ' + errorType.description + ' ' + path;
-	var error = new Error(message);
-	error.errno = errorType.errno;
-	error.code = errorType.code;
-	error.path = path;
-	return error;
-}
